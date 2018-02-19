2518d91338e579d5edc92cfa0d98d6d6cfa2e3c4
WilliamJPalmer <deity@mac.com>
2018-02-18 22:19:55 -0600

    updated code for testing

--- imports/api/users.js
+++ imports/api/users.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import { Accounts } from 'meteor/accounts-base';
 
-Accounts.validateNewUser((user) => {
+export const validateNewUser = (user) => {//need to export the validateNewUser for testing.
   const email = user.emails[0].address;
 
   new SimpleSchema({
@@ -12,6 +12,22 @@ Accounts.validateNewUser((user) => {
     }
   }).validate({email:email});//ES6 shorthand lets this be written as .validate({ email })
 
-
   return true;
-});
+}
+ if(Meteor.isServer){//this will run when it is on the server and not the client.
+   Accounts.validateNewUser(validateNewUser);
+ };
+
+/*code above refactored for testing*/
+// Accounts.validateNewUser((user) => {
+//   const email = user.emails[0].address;
+//
+//   new SimpleSchema({
+//     email: {
+//       type: String,
+//       regEx: SimpleSchema.RegEx.Email
+//     }
+//   }).validate({email:email});//ES6 shorthand lets this be written as .validate({ email })
+//
+//   return true;
+// });
