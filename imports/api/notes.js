14784f65524d64a4911367ce87360ed43d554538
WilliamJPalmer <deity@mac.com>
2018-02-19 23:26:00 -0600

    added updating functionality and test cases for functionality

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,5 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 import moment from 'moment';
 
 export const Notes = new Mongo.Collection('notes');
@@ -14,6 +15,49 @@ Meteor.methods({
       body:'',
       userId: this.userId,
       updatedAt: moment().valueOf()
+    });
+  },
+  'notes.remove'(_id){
+    if(!this.userId){
+      throw new Meteor.Error('user not Authorized')
+    }
+    new SimpleSchema ({
+      _id: {
+        type: String, // makes usre the url id is a string. Must be String and not string
+        min: 1 // makes sure id length is at least 1 character long.
+      }
+    }).validate({_id });
+    Notes.remove ({ _id , userId: this.userId });
+  },
+  'notes.update'(_id, updates){//_id is the id of the note to update. "updates" is an object that has body and title, what is in the simple Schema
+    if(!this.userId){
+      throw new Meteor.Error('user not Authorized')
+    }
+    new SimpleSchema ({
+      _id: {
+        type: String,
+        min: 1
+      },
+      title: {
+        type: String,
+        optional: true
+      },
+      body: {
+        type: String,
+        optional :true
+      }
+    }).validate({
+      _id,
+      ...updates// this spread operator will validate the title and/or body objects and error is anything else added. Anything that is added outside of this code, possible malicious inserts, will error out as only, -id, title and body are valid.
+    });
+    Notes.update({
+      _id,
+      userId: this.userId//adding this makes sure that the creator is updating the note.
+    }, {
+      $set: {//$set updates the entry while not erasing the orther entries.
+        updatedAt: moment().valueOf(),
+        ...updates//any changes to the title or body have been validated and will be updated here.
+      }
     });
   }
 });
--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,14 +5,27 @@ import { Notes } from './notes';
 
 if(Meteor.isServer){
   describe('notes', function(){
+    const noteOne = {//setting noteOne = the object below allows updating the information to be done more easily
+      _id: 'testNoteId1',
+      title: 'myTitle',
+      body: 'My body for note',
+      updatedAt: 0,
+      userId: 'testUserId1'
+    };
+    beforeEach(function() {
+      Notes.remove({});//this will remove all notes in the db.
+      /* When running Meteor in test mode, it is using a separate databse. This means
+      that Notes.remove ({}) will not delete development data. All development data is intact.*/
+      Notes.insert(noteOne);
+    });
     it('should insert new note', function(){
-      const userId = 'testId';
+      const userId = 'testid'
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId: userId });//can be .apply({ userId }) in ES6
       /* Meteor.server.method_handlers['notes.insert'] is the same method as what
       is defined in notes.js after 'notes.insert'. the [] are necessary in the
       Meteor.server.method_hbndlers['notes.insert'] because the . is a special character
       Apply is a javascript feature that is a way of calling a function specifying your own
-      this centext.
+      this context.
       */
       expect(Notes.findOne({ _id, userId: userId })).toExist();//can be .finOne({ _id, userId }) in ES6
     });
@@ -22,6 +35,74 @@ if(Meteor.isServer){
         Meteor.server.method_handlers['notes.insert']();
       }).toThrow();
     });
+    it('should remove note', function(){
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
+      /* .array takes two arguments in this case. The first is the object that is set to the this context.
+      The second argument is an array and every value in the array is passed into the function as an argument.
+      since the note is being deleted by the _id, only need to pass that in the array. This is because the
+      notes.remove function in notes.js is only using the _id as an argument.*/
+      expect(Notes.findOne({ _id: noteOne._id})).toNotExist();
+    });
+    it('shold not remove note if not authenticated', function (){
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);//tests to see if can remove note w/o userId
+      }).toThrow();
+    });
+    it('should not remove note when no note ID', function(){
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
+      }).toThrow();
+    });
+    it('should update note', function(){
+      const title = "Updated title here"
+      Meteor.server.method_handlers['notes.update'].apply({
+        userId: noteOne.userId//the is the this context object
+      }, [
+        noteOne._id,
+        { title }//this array has two values that are passed as arguments since the title is being changed. Const noteOne
+      ]);
+      const note = Notes.findOne(noteOne._id);//to make sure update worked, need ot return note that was updated.
 
+      expect(note.updatedAt).toBeGreaterThan(0);// 0 is default for updated at, is also 1970, value should be greater than 0.
+      expect(note).toInclude({
+        title,//this is the new Title, defined in the vairable "title" above
+        body : noteOne.body// body should be the same as the seed data in the variable noteOne
+      });
+    });
+
+    it('should throw error if extra updates', function () {
+      const name = "William";
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({
+          userId: noteOne.userId
+        }, [
+          noteOne._id,
+          { name },//name is not a field in the note object so an error would occur.
+        ]);
+      }).toThrow();
+    });
+
+    it('should not update note if user was not creator', function (){
+      const title = "Updated title here"
+      Meteor.server.method_handlers['notes.update'].apply({
+        userId: "FakeUser"//the is the this context object
+      }, [
+        noteOne._id,
+        { title }//this array has two values that are passed as arguments since the title is being changed. Const noteOne
+      ]);
+      const note = Notes.findOne(noteOne._id);//to make sure update worked, need ot return note that was updated.
+
+      expect(note).toInclude(noteOne);
+    });
+    it('shold not update note if not authenticated', function (){
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({}, [noteOne._id]);
+      }).toThrow();
+    });
+    it('should not update note when no note ID', function(){
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId });
+      }).toThrow();
+    });
   });
 }
