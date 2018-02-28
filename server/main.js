b978ac88ead93532d4e0532b2be0ca7663926ad4
WilliamJPalmer <deity@mac.com>
2018-02-27 22:58:59 -0600

    added import for api/notes

--- server/main.js
+++ server/main.js
@@ -3,11 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';//this gives access to the webapp behind the scenes
 //that serves the content. Allows allows to attach middleware.
 
-
-import '../imports/startup/simple-schema-configuration';
-
 import '../imports/api/users';
-
+import '../imports/api/notes';
+import '../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
 
