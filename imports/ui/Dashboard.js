921553c4d84b79283e4289ab89d9e6cb2023118f
WilliamJPalmer <deity@mac.com>
2018-02-27 23:03:55 -0600

    added NoteList import and component

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import PrivateHeader from './PrivateHeader';//don't need braces here because we are importing. This is the containerized component
+import NoteList from './NoteList';
+// import NoteListHeader from './NoteListHeader';
 
 
 export default () => {
@@ -9,12 +11,17 @@ export default () => {
       <PrivateHeader title="Dashboard"/>
       <div className="page-content">
         Dashboard page content
+        <NoteList/>
       </div>
     </div>
-  )
-}
+  );
+};
 /*container is passed in via <PrivateHeader/> with the required title prop. the
 PrivateHeader component also requires the handleLogout prop, which contains the logout function.
 The handleLogout comes from the container component and it runs the funtion through trackerAutorun
 and anything that gets return is passed on to the presentational component as a prop.
-*/
+
+
+for the NotesList, this works the same. Don't need to have the notelist array in the
+JSX here. that is because it is returned in the container in NoteList.js and that is
+passed into the named export NoteList, which is imported at the top.*/
