d4ea31c52115b7e924dd174e644d9f5ca5b5602d
WilliamJPalmer <deity@mac.com>
2018-02-28 18:21:59 -0600

    imported the NoteListEmptyItem and added ternary operator to display notes or NoteListEmptyitem if zero notes.

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -6,15 +6,16 @@ import { Notes } from '../api/notes';
 //will also need to import notes, like users, in server/main.js
 import NoteListHeader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
+import NoteListEmptyItem from './NoteListEmptyItem';
 
 export const NoteList = (props) => {
   return (
     <div>
       <NoteListHeader/>
+      { props.notes.length === 0 ? <NoteListEmptyItem/> : undefined }
       {props.notes.map((note) => {
         return <NoteListItem key={note._id} note={note}/>
       })}
-
 
       Note List { props.notes.length }
     </div>
