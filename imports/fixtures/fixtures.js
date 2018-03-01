8e24eb3e34d77b80599f406627904b4202fac958
WilliamJPalmer <deity@mac.com>
2018-02-28 23:45:27 -0600

    new file for test data

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+  {
+    _id: 'noteId1',
+    title: 'Test Title',
+    body: '',
+    updatedAt: 1519792858675,
+    userId: 'userId1'
+  }, {
+    _id: 'noteId2',
+    title: '',
+    body: 'This is the body for this note',
+    updatedAt: 1519792858675,
+    userId: 'userId2'
+  }
+];
