e58c0660135ec23f011eaae0be464dc43bef6b9c
WilliamJPalmer <deity@mac.com>
2018-03-02 16:24:48 -0600

    added Session elements.

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if(res){//if there is a response to creating a new note
+            props.Session.set('selectedNoteId', res)//sets the selectedNoteId to the id of the newly created note.
+          }
+        });
       }}>Create Note</button>
     </div>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: React.PropTypes.func.isRequired
+  meteorCall: React.PropTypes.func.isRequired,
+  Session: React.PropTypes.object.isRequired//incliding Session gives access to the meteorCall 'notes.insert'
 }
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session: Session
   }
 }, NoteListHeader);
