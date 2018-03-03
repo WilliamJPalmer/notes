2424bb33db22b1beda936a209999ac2853b7b568
WilliamJPalmer <deity@mac.com>
2018-03-03 17:20:51 -0600

    added session to Meteor.startup and added new Tracker.autorun for session

--- client/main.js
+++ client/main.js
@@ -17,18 +17,23 @@ Tracker.autorun(() => {
   onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
-Session.set()
-
 Tracker.autorun(() => {
     const selectedNoteId = Session.get('selectedNoteId');
+    Session.set('isNavOpen', false);//hides the menu after clicking a note in the list or creating a new note.
     if (selectedNoteId){
-      browserHistory.replace(`/dashboard/${selectedNoteId}`)
+      browserHistory.replace(`/dashboard/${selectedNoteId}`);
+
     }
 });
 /*this Tracker.autorun looks for a change in the value of selectedNoteId and when there is a change,
 the url will be updated.*/
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+  document.body.classList.toggle('is-nav-open', isNavOpen);//this targets a class in the body and checks if it exists based on boolean value of variable isNavOpen. If the value is true, the class 'is-nave-open' will be added. If the value is false, the class 'is-nav-open' will be removed. This allows for this toggled class to be targeted by styles and other code. when the class exists, we can show, style or do something.
+});
 
 Meteor.startup(() => {
   Session.set('selectedNoteId', undefined);//this is the Meteor Session Variable that will be used for updating notes.
+  Session.set('isNavOpen', false);//the navigation, sidebar, should not show up when app is loaded.
   ReactDOM.render(routes, document.getElementById('app'));
 });
