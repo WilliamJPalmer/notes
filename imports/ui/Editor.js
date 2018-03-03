0fc54bfbbae4ad904303cc65c2360774ae7f911e
WilliamJPalmer <deity@mac.com>
2018-03-02 18:14:46 -0600

    added styles and update components to use styles

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -57,7 +57,7 @@ export class Editor extends React.Component {
     // }
     if(this.props.note){//if a note is found with the id
       return(
-        <div>
+        <div className="editor">
           <input value={this.state.title} placeholder='Your Note Title' onChange={this.handleTitleChange.bind(this)}/>
           <textarea value={this.state.body} placeholder='Your Note Content' onChange={this.handleBodyChange.bind(this)}></textarea>
           <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
@@ -65,9 +65,11 @@ export class Editor extends React.Component {
       );
     } else {
       return (
-        <p>
-          {this.props.selectedNoteId ? 'Note Not Found' : 'Please select or create a Note to start'}
-        </p>
+        <div className="editor">
+          <p>
+            {this.props.selectedNoteId ? 'Note Not Found' : 'Please select or create a Note to start'}
+          </p>
+        </div>
       );//if the selectedNoteId in URL not found, first message. If selectedNoteId empty string, second message. This
     }// ternary operator takes the places of the else if and else in the code commented out above.
   }
